6d53993b2ac1cf5b21ff220d9ed60ca7e685c32f
Aleksandr Filatov <v-alfila@microsoft.com>
2017-02-20 10:50:40 -0800

    fix onPause

--- www/ondestroyplugin.js
+++ www/ondestroyplugin.js
@@ -2,8 +2,10 @@ module.exports = function () {
  exports.setEventListener = exports.setDestroyEventListener = function (callback) {
   cordova.exec (callback, undefined, "OnDestroyPlugin", "setEventListener", [])
  }
- exports.setOnPauseEventListener = function (callback) {
-  cordova.exec (callback, undefined, "OnPausePlugin", "setEventListener", [])
+ exports.addOnPauseEventListener = function (callback) {
+  document.addEventListener("deviceready", function () {
+   document.addEventListener("pause", callback, false)
+  })
  }
  return exports
 } ()
